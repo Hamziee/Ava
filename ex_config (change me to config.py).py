dac2bd97cfa9dd8115475d82a9cbda05c80cb3bc
Hamza <hamza@heo-systems.com>
2024-02-22 14:20:24 +0100

    Update ex_config (change me to config.py).py

--- ex_config (change me to config.py).py
+++ ex_config (change me to config.py).py
@@ -3,3 +3,4 @@ OWNER_ID = 496673945211240462 # copied from my old bot, unused for now
 PERMISSION_DENIED = 'You do not have permission to run this command.' # copied from my old bot, unused for now
 STATUS = "v0.1-b"
 TOKEN = 'Put your Discord bot token here.'
+COMMANDS_DIRECTORY = 'commands'
