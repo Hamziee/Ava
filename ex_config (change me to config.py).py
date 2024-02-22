1f51fd5f898bdee2883e078d1aedd24ca27829ab
Hamza <hamza@heo-systems.com>
2024-02-22 01:28:52 +0100

    Update ex_config (change me to config.py).py

--- ex_config (change me to config.py).py
+++ ex_config (change me to config.py).py
@@ -1,4 +1,4 @@
-PREFIX = '$'
+PREFIX = '$' # deprecated only used for admin or legacy commands.
 OWNER_ID = 496673945211240462
 PERMISSION_DENIED = 'You do not have permission to run this command.'
 STATUS = "v0.1-b"
