0e43077d8d91888c23e6fc2f161ee582b2f779a7
Hamza <hamza@heo-systems.com>
2024-02-24 20:01:14 +0100

    version change

--- ex_config (change me to config.py).py
+++ ex_config (change me to config.py).py
@@ -1,6 +1,6 @@
 PREFIX = '$' # deprecated only used for admin or legacy commands.
 OWNER_ID = 496673945211240462 # copied from my old bot, unused for now
 PERMISSION_DENIED = 'You do not have permission to run this command.' # copied from my old bot, unused for now
-STATUS = "v0.1.1-b"
+STATUS = "v0.1.2-b"
 TOKEN = 'Put your Discord bot token here.'
 COMMANDS_DIRECTORY = 'commands'
