106444e92fdc06a7530f56ebb720652f9939d0aa
Hamza <hamza@heo-systems.com>
2024-02-25 15:18:57 +0100

    Update ex_config (change me to config.py).py

--- ex_config (change me to config.py).py
+++ ex_config (change me to config.py).py
@@ -1,8 +1,8 @@
 MIA_VERSION='v0.1.4-b' # Do not change this, it will help with troubleshooting later
 
-PREFIX = '$' # deprecated, only used for admin or music commands.
-OWNER_ID = 496673945211240462 # used for admin commands and ChatAI recognition
-PERMISSION_DENIED = 'You do not have permission to run this command.' # copied from my old bot, unused for now
-STATUS = MIA_VERSION # Want a custom status? Replace it with: STATUS = 'your status here'
+PREFIX = '$' # deprecated, only used for admin and music commands.
+OWNER_ID = 496673945211240462 # used for admin commands and ChatAI recognition.
+PERMISSION_DENIED = 'You do not have permission to run this command.' # this message will popup when a normal user runs a admin command.
+STATUS = MIA_VERSION # Want a custom status? Replace it with: STATUS = 'your status here'.
 TOKEN = 'Put your Discord bot token here.'
 COMMANDS_DIRECTORY = 'commands'
