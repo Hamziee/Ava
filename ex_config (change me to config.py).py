02fd0cbc93601c14c22f817f6d02267224b965cc
Hamza <hamza@heo-systems.com>
2024-04-08 21:14:01 +0200

    Update ex_config (change me to config.py).py

--- ex_config (change me to config.py).py
+++ ex_config (change me to config.py).py
@@ -3,6 +3,6 @@ AVA_VERSION='v0.1.9-b' # Do not change this, it will help with troubleshooting l
 PREFIX = '$' # deprecated, only used for admin and music commands.
 OWNER_ID = 496673945211240462 # used for admin commands and ChatAI recognition.
 PERMISSION_DENIED = 'You do not have permission to run this command.' # this message will popup when a normal user runs a admin command.
-STATUS = MIA_VERSION # Want a custom status? Replace it with: STATUS = 'your status here'.
+STATUS = AVA_VERSION # Want a custom status? Replace it with: STATUS = 'your status here'.
 TOKEN = 'Put your Discord bot token here.'
 COMMANDS_DIRECTORY = 'commands'
