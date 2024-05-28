95d2e8008d16bbcd05ceca782d00431a70587bf8
Hamza <hamza@heo-systems.com>
2024-05-28 02:24:55 +0200

    made a variable for footer icon

--- commands/about.py
+++ commands/about.py
@@ -14,7 +14,7 @@ class about(commands.Cog):
                 color=discord.Colour.blurple(),
                 title="Hi I'm Ava! <:Ava_CatBlush:1210004576082853939>",
                 description="I am an versatile and open-source Discord bot designed to serve all your needs effortlessly. With Ava by your side, managing your server becomes a breeze. From moderation tools to fun utilities, Ava has it all covered. Embrace simplicity and functionality with Ava, the reliable companion for any Discord community.\n### [Request Feature](https://github.com/Hamziee/Ava/issues/new) **|** [Github](https://github.com/Hamziee/Ava>) **|** [Maintainer](<https://github.com/Hamziee/>) \n[Hosted by HEO Systems](<https://heo-systems.net>)\n\n**Developer Note:** Ava is currently a work in progress, and I am working on adding new and exciting features to Ava. Please bear in mind that this is a project of mine that I work on in my free time, so don't expect fast progress. - Hamza")
-            embed.set_footer(text=f"Ava | version: {config.AVA_VERSION}", icon_url="https://cdn.discordapp.com/avatars/1209925239652356147/38e76bc9070eb00f2493b6edeab22b33.webp")
+            embed.set_footer(text=f"Ava | version: {config.AVA_VERSION}", icon_url=config.FOOTER_ICON)
             await interaction.response.send_message(embed=embed)
         except Exception as e:
             print(e)
--- commands/hug.py
+++ commands/hug.py
@@ -9,7 +9,7 @@ class hug(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
 
-    @app_commands.command(name="hug", description="Hug your (imaginary) friend/lover!")
+    @app_commands.command(name="hug", description="Hug your friend/lover!")
     @app_commands.rename(member='person')
     async def hug(self, interaction: discord.Interaction, member: discord.Member):
         try:
@@ -22,9 +22,18 @@ class hug(commands.Cog):
                     color=discord.Colour.blurple()
                 )
                 embed.set_image(url=image_url)
-                embed.set_footer(text=f"Ava | version: {config.AVA_VERSION} - Image by: api.hamzie.site", icon_url="https://cdn.discordapp.com/avatars/1209925239652356147/38e76bc9070eb00f2493b6edeab22b33.webp")
-
-                await interaction.response.send_message(content= f"{interaction.user.mention} hugs {member.mention}", embed=embed)
+                embed.set_footer(text=f"Ava | version: {config.AVA_VERSION} - Image by: api.hamzie.site", icon_url=config.FOOTER_ICON)
+                
+                if member.id == interaction.user.id:
+                    await interaction.response.send_message(content="You can't hug yourself! Here I will give you a big hug! <a:ava_CatCuddle:1244799986600902758>", embed=embed)
+                    return
+                if member.bot:
+                    await interaction.response.send_message(content="Bots don't need hugs! But I do, so here is a hug for you! <a:AVA_cats_hug:1244799755960451159>", embed=embed)
+                    return
+                if member.id == config.BOT_ID:
+                    await interaction.response.send_message(content="Here I will give you a big hug! <a:AVA_hugg:1244799800378130472>", embed=embed)
+                    return
+                await interaction.response.send_message(content= f"{interaction.user.mention} hugs {member.mention} <:AVA_hug:1244799683042344970>", embed=embed)
         except httpx.HTTPError as http_err:
             print(f'HTTP error occurred: {http_err}')
         except Exception as err:
