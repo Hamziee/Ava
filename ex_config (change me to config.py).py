9cf8448c6977858783704d693606e7c3a1daccb4
Hamza <hamza@heo-systems.com>
2024-05-28 02:25:27 +0200

    extra info + needed config values

--- ex_config (change me to config.py).py
+++ ex_config (change me to config.py).py
@@ -1,14 +1,19 @@
-AVA_VERSION='v0.3.0-beta' # Do not change this, it will help with troubleshooting later
+AVA_VERSION = 'v0.4.0-dev' # Do not change this, it will help with troubleshooting later
+CONFIG_VERSION = 3 # Do not change this, it will help with troubleshooting later
 
 # Required Bot Configuration
+# Configuring this to your bot's settings is crucial for the bot to function properly.
 
 PREFIX = '$' # deprecated, only used for admin or music commands.
 OWNER_ID = 496673945211240462 # used for admin commands and ChatAI recognition
 PERMISSION_DENIED = 'You do not have permission to run this command.' # copied from my old bot, unused for now
 STATUS = AVA_VERSION # Want a custom status? Replace it with: STATUS = 'your status here'
+FOOTER_ICON = 'https://cdn.discordapp.com/avatars/1209925239652356147/38e76bc9070eb00f2493b6edeab22b33.webp' # Put your bot's avatar URL here or an image of your choice
+BOT_ID = 1209925239652356147 # Put your bot's ID here
 TOKEN = 'Put your Discord bot token here.'
 COMMANDS_DIRECTORY = 'commands'
 
 # Optional Bot Configuration
+# Configuring this to your bot's settings is optional, but it can enhance the bot's functionality. Make sure to disable commands that require these settings if you don't configure them.
 
 THECATAPI_KEY = 'your thecatapi key here' # Get your own free key at https://thecatapi.com/
